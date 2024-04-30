cadb114619f846a39d7222e84d62dc47ce98f173
Q8er2y <155814328+Q8er2y@users.noreply.github.com>
2024-04-30 21:46:38 +0100

    Create AVitems.java

---
+++ src/Q8er2y/AdvV/content/AVitems.java
@@ -0,0 +1,31 @@
+package AV.content;
+
+import arc.graphics.*;
+import arc.struct.Seq;
+import q8er2y.javaav.AVMod;
+import AV.graphics.*;
+import mindustry.type.Item;
+
+import static mindustry.content.Items.*;
+
+public class AVItems {
+    public static Item titaniumCluster;
+
+    public static Seq<Item> uxerdItems = new Seq<>(), lumoniItems = new Seq<>();
+
+    public static void load(){
+        titaniumCluster = new Item("Titanium-Cluster", Color.valueOf("3d7fa1")){{
+            hardness = 3;
+            cost = 1f;
+        }};
+       
+            @Override
+            public void loadIcon(){
+  
+            }
+        };
+
+     
+        serpuloItems.addAll(titaniumCluster, t);
+    }
+}
