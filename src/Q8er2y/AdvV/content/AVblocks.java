752b00f10f15ec01ec011987d00ba1596212b70e
Q8er2y <155814328+Q8er2y@users.noreply.github.com>
2024-05-02 10:22:05 +0100

    Create AVblocks.java

---
+++ src/Q8er2y/AdvV/content/AVblocks.java
@@ -0,0 +1,15 @@
+package .content;
+
+import omaloon.content.blocks.*;;
+
+public class OlBlocks {
+    public static void load(){
+        OlEnvironmentBlocks.load();
+        OlDefenceBlocks.load();
+        OlStorageBlocks.load();
+        OlProductionBlocks.load();
+        OlDistributionBlocks.load();
+        OlPowerBlocks.load();
+        OlCraftingBlocks.load();
+    }
+}
